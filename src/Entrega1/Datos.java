f3659a55306635c065a2f6ef65e01bf59e3d38c5
Ale <alejandromaximo2000@gmail.com>
2022-03-27 19:55:12 -0300

    First

---
+++ src/Entrega1/Datos.java
@@ -0,0 +1,14 @@
+package Entrega1;
+
+public class Datos {
+    public static void main(String[] args) {
+
+        String nombre = "Juan Perez";
+        int edad = 45;
+        long dni = 54347655;
+        double peso = 72.4;
+        boolean casado = true;
+        char inicialNombre = J;
+
+    }
+}
